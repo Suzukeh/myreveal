49deca0709e410ee77eeaad763b1156b82c88ae5
Suzuke <49985092+Suzukeh@users.noreply.github.com>
2022-02-26 15:08:05 +0900

    update

--- fix/themeURL.py
+++ fix/themeURL.py
@@ -1,14 +1,21 @@
 import sys
-import pathlib
+from pathlib import Path
 import os
+
 
 args = sys.argv
 
-arg = args[0]
+arg = os.path.abspath(args[1])
 
 if (arg == '--help') or (arg == '-h'):
     print("fix custom CSS path\n\t> themeURL.py foo.html")
     sys.exit()
 
 
-htmlpath = pathlib.Path("./../theme/")
+htmlpath = Path(arg)
+print(htmlpath)
+
+content = htmlpath.read_text()
+content = content.replace('href=\"/_assets/theme/',
+                          'href=\"./../_assets/theme/')
+htmlpath.write_text(content)
