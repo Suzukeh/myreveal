ea9691af08cbfe23b75478dca55974c65301bccb
Suzuke <49985092+Suzukeh@users.noreply.github.com>
2022-02-26 14:26:08 +0900

    update

--- fix/addfiles.sh
+++ fix/addfiles.sh
@@ -1,6 +1,9 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo $SCRIPT_DIR
-cp ./.nojekyll $SCRIPT_DIR/../docs/
+cp $SCRIPT_DIR/.nojekyll $SCRIPT_DIR/../docs/
 
 rm -rf $SCRIPT_DIR/../docs/_assets/plugin
+rm -rf $SCRIPT_DIR/../docs/favicon.ico
+
+cp $SCRIPT_DIR/favicon.ico $SCRIPT_DIR/../docs/favicon.ico
 cp -r $SCRIPT_DIR/../plugin $SCRIPT_DIR/../docs/_assets/plugin
