2d0ff761588578dadd4ff1e6142651912c5791ed
Suzuke <49985092+Suzukeh@users.noreply.github.com>
2022-12-02 04:46:09 +0900

    update

--- static.sh
+++ static.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$1
 realpath $SCRIPT_DIR
 
 cp $SCRIPT_DIR/docs/CNAME $SCRIPT_DIR/fix/CNAME
-reveal-md $SCRIPT_DIR/md/ --static docs --glob '**/*_slides.md '
+reveal-md $SCRIPT_DIR/md/ --static docs --glob '**/*_slides.md'
 
 $SCRIPT_DIR/fix/addfiles.sh $SCRIPT_DIR
 python3 $SCRIPT_DIR/fix/themeURL.py $SCRIPT_DIR/docs/
